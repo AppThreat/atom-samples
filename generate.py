1554f56d5550680abf77edb04ddc556910fed3d3
Caroline Russell <caroline@appthreat.dev>
2023-12-14 12:14:16 -0500

    Fix: Issue 5 sdkman/script issues in generation workflow (#6)
    Don't install axios dependencies
    
    
    
    Correction
    
    
    
    Correction
    
    
    
    Correction
    
    
    
    Correction
    
    
    
    Workflow refinements
    
    
    
    Troubleshoot cleanup.
    
    
    
    Don't install dependencies for spaCy as it takes to long to get reachables.
    
    
    
    Corrected cleanup
    
    
    
    Add cleanup function.
    
    
    
    Resolved workflow migration issues.
    
    Change workflow conditionals
    
    
    
    Corrected reference.
    
    
    
    Corrected syntax error.
    
    
    
    Resolving slice generation errors.
    
    Signed-off-by: Caroline Russell <caroline@appthreat.dev>

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 import csv
 import logging
+import pathlib
 import subprocess
 import os
 import argparse
@@ -9,7 +10,7 @@ from pathlib import Path
 
 def build_args():
     """
-    Builds and returns the argument parser for the program.
+    Builds the argument parser for the command line interface (CLI).
     """
     parser = argparse.ArgumentParser()
     parser.set_defaults(slice_types=['usages', 'reachables'])
@@ -65,7 +66,7 @@ def build_args():
     parser.add_argument(
         '--skip-clone',
         action='store_false',
-        dest='clone',
+        dest='skip_clone',
         default=True,
         help='Skip cloning the repositories (must be used with the --repo-dir '
              'argument)'
@@ -74,6 +75,7 @@ def build_args():
         '--debug-cmds',
         action='store_true',
         dest='debug_cmds',
+        help='For use in workflow'
     )
     parser.add_argument(
         '--skip-build',
@@ -83,67 +85,93 @@ def build_args():
         help='Skip building the samples and just run atom. Should be used '
              'with --skip-clone'
     )
+    parser.add_argument(
+        '--cleanup',
+        action='store_true',
+        dest='cleanup',
+        default=False,
+        help='Remove slices that are 1kb or less in size (successful slices '
+             'are always larger than 1kb)'
+    )
     return parser.parse_args()
 
 
-def generate(repo_data, clone_dir, output_dir, slice_types, clone, debug_cmds,
-             skip_build):
+def generate(args):
     """
-    Iterates over sample repositories and gets commands for each.
+    Generate commands for executing a series of tasks on a repository.
 
     Args:
-        repo_data (list): Repository data.
-        clone_dir (pathlib.Path): The directory to clone the repositories.
-        output_dir (pathlib.Path): The directory to output the slices.
-        slice_types (list): Slice types to generate.
-        clone (bool): Indicates whether to clone the repositories or not.
-        debug_cmds (bool): Indicates whether to include print commands.
-        skip_build (bool): Indicates whether to skip the pre-build step or not.
+        args (argparse.Namespace): The parsed arguments
 
     Returns:
         None
     """
-    if not skip_build:
-        run_pre_builds(repo_data, output_dir, debug_cmds)
+    langs = set(args.langs)
+    if args.output_dir == '.':
+        args.output_dir = pathlib.Path.cwd()
+    if args.elangs:
+        langs = langs - set(args.elangs)
+    if not args.debug_cmds:
+        check_dirs(args.skip_clone, args.clone_dir, args.output_dir)
 
-    commands = '\nsdk use java 20.0.2-tem'
+    repo_data = read_csv(args.repo_csv, langs)
 
-    for repo in repo_data:
-        commands += generate_slices(
-            clone, clone_dir, output_dir, repo, skip_build, slice_types)
-        commands += '\n\n'
+    if not args.skip_build:
+        run_pre_builds(repo_data, args.output_dir, args.debug_cmds)
 
-    sh_path = Path.joinpath(output_dir, 'atom_commands.sh')
-    use_script(sh_path, commands, debug_cmds)
+    commands = ''.join(
+        exec_on_repo(
+            args.skip_clone,
+            args.clone_dir,
+            args.output_dir,
+            args.skip_build,
+            args.slice_types,
+            args.debug_cmds,
+            repo,
+        )
+        for repo in repo_data
+    )
+    sh_path = Path.joinpath(args.output_dir, 'atom_commands.sh')
+    write_script_file(sh_path, commands, args.debug_cmds)
 
 
-def generate_slices(clone, clone_dir, output_dir, repo, skip_build,
-                    slice_types):
+def exec_on_repo(
+        clone,
+        clone_dir,
+        output_dir,
+        skip_build,
+        slice_types,
+        debug_cmds,
+        repo
+):
     """
-    Generates slices for a given clone of a repository.
+    Determines a sequence of commands on a repository.
 
     Args:
         clone (bool): Indicates whether to clone the repository.
-        clone_dir (pathlib.Path): The directory where the clone is stored.
-        output_dir (pathlib.Path): The directory for the generated slices.
-        repo (dict): A dictionary containing information about the repository.
-        skip_build (bool): Indicates whether to skip the build step.
-        slice_types (list): A list of slice types to generate.
+        clone_dir (pathlib.Path): The directory to store sample repositories.
+        output_dir (pathlib.Path): The directory to output the slices.
+        skip_build (bool): Indicates whether to skip the build phase.
+        slice_types (list): The types of slices to be generated.
+        debug_cmds (bool): Indicates whether to include debug output.
+        repo (dict): The repository information.
+
 
     Returns:
-        str: The commands to generate the slices for a single repository.
+        str: The sequence of commands to be executed.
     """
     project = repo['project']
     lang = repo['language']
-    loc = os.getcwd()
+    loc = Path.cwd()
     repo_dir = Path.joinpath(clone_dir, lang, project)
     commands = ''
-    if clone:
+
+    if clone and not debug_cmds:
         clone_repo(repo['link'], clone_dir, repo_dir)
     commands += f"\n{subprocess.list2cmdline(['cd', repo_dir])}"
     if not skip_build and len(repo['pre_build_cmd']) > 0:
         cmds = repo['pre_build_cmd'].split(';')
-        cmds = [cmd.strip() for cmd in cmds]
+        cmds = [cmd.lstrip().rstrip() for cmd in cmds]
         for cmd in cmds:
             new_cmd = list(cmd.split(' '))
             commands += f"\n{subprocess.list2cmdline(new_cmd)}"
@@ -157,25 +185,25 @@ def generate_slices(clone, clone_dir, output_dir, repo, skip_build,
     if lang == 'java':
         commands += '\nsdk use java 20.0.2-tem'
     for stype in slice_types:
-        slice_file = Path.joinpath(
-            output_dir, lang, f"{project}-{stype}.json")
+        slice_file = Path.joinpath(output_dir, lang, f"{project}-{stype}.json")
         atom_file = Path.joinpath(repo_dir, f"{project}.atom")
         cmd = ['atom', stype, '-l', lang, '-o', atom_file, '-s', slice_file,
                repo_dir]
         commands += f"\n{subprocess.list2cmdline(cmd)}"
+    commands += '\n\n'
     return commands
 
 
 def read_csv(csv_file, langs):
     """
-    Read a CSV file and filters the data based on the provided languages.
+    Reads a CSV file and filters the data based on a list of languages.
 
     Parameters:
         csv_file (pathlib.Path): The path to the CSV file.
-        langs (set): A list of languages to filter the data by.
+        langs (set): A set of programming languages.
 
     Returns:
-        list: Repository list filtered by the provided languages.
+        list: A filtered list of repository data.
     """
     with open(csv_file, 'r', encoding='utf-8') as f:
         reader = csv.DictReader(f)
@@ -189,35 +217,36 @@ def clone_repo(url, clone_dir, repo_dir):
     """
     Clones a repository from a given URL to a specified directory.
 
-    Parameters:
+    Args:
         url (str): The URL of the repository to clone.
-        clone_dir (pathlib.Path): Parent directory for all cloned repositories.
-        repo_dir (pathlib.Path): Subdirectory of clone_dir for a single repo.
+        clone_dir (pathlib.Path): The directory to clone the repository into.
+        repo_dir (pathlib.Path): The directory to store the cloned repository.
 
     Returns:
         None
     """
     os.chdir(clone_dir)
     if Path.exists(repo_dir):
-        logging.warning(f'{repo_dir} already exists, skipping clone.')
+        logging.info('%s already exists, skipping clone.', repo_dir)
         return
-    clone_cmd = ['git', 'clone', url, repo_dir]
+
+    clone_cmd = f'git clone {url} {repo_dir}'
     subprocess.run(clone_cmd, shell=True, encoding='utf-8', check=False)
 
 
 def run_pre_builds(repo_data, output_dir, debug_cmds):
     """
-    Generate commands for pre-build steps.
+    Generates a list of commands to be executed before the build process.
 
-    Parameters:
-    - repo_data: Contains repository data.
-    - output_dir (pathlib.Path): The output directory path.
-    - debug_cmds: If True, print the commands to the console.
+    Args:
+        repo_data (list[dict]): Contains the sample repository data
+        output_dir (pathlib.Path): Root directory for slices export
+        debug_cmds (bool): Flag indicating whether to include debug output
 
     Returns:
-    None
+        None
     """
-    cmds = []
+    cmds = ['sdk install java 20.0.2-tem']
     [
         cmds.extend(row['pre_build_cmd'].split(';'))
         for row in repo_data
@@ -229,73 +258,75 @@ def run_pre_builds(repo_data, output_dir, debug_cmds):
     commands = [c.replace('use', 'install') for c in cmds]
     commands = '\n'.join(commands)
     sh_path = Path.joinpath(output_dir, 'sdkman_installs.sh')
-    use_script(sh_path, commands, debug_cmds)
+    write_script_file(sh_path, commands, debug_cmds)
 
 
-def use_script(file_path, commands, debug_cmds):
+def write_script_file(file_path, commands, debug_cmds):
     """
-    Write the given commands to a file specified by `file_path`.
+    Write a script to execute a series of commands in a file.
 
     Args:
-        file_path (pathlib.Path): File where the commands will be written.
-        commands (str): The commands to be written to the file.
-        debug_cmds (bool): If True, print the commands to the console.
+        file_path (pathlib.Path): The path to write the file to
+        commands (str): The commands to be written to the file
+        debug_cmds (bool): Flag indicating whether to include debug output
+
+    Returns:
+        None
     """
     with open(file_path, 'w', encoding='utf-8') as f:
-        sdkman_path = os.path.join('$SDKMAN_DIR', 'bin', 'sdkman-init.sh')
+        sdkman_path = Path.joinpath(
+            Path('$SDKMAN_DIR'), 'bin', 'sdkman-init.sh')
         f.write(f'#!/usr/bin/bash\nsource {sdkman_path}\n\n')
-        f.write('sdk use java 20.0.2-tem\n')
         f.write(commands)
     if debug_cmds:
-        logging.info(commands)
+        print(commands)
 
 
-def check_dirs(clone_dir, output_dir):
+def check_dirs(clone, clone_dir, output_dir):
     """
     Create directories if they don't exist.
 
     Args:
-        clone_dir (pathlib.Path): Path to the clone directory.
-        output_dir (pathlib.Path): Path to the output directory.
+        clone (bool): Whether to create the clone directory or not.
+        clone_dir (pathlib.Path): The path to the clone directory.
+        output_dir (pathlib.Path): The path to the output directory.
+
+    Returns:
+        None
     """
-    if not Path.exists(clone_dir):
+    if clone and not Path.exists(clone_dir):
         Path.mkdir(clone_dir)
     if not Path.exists(output_dir):
         Path.mkdir(output_dir)
 
 
+def cleanup(output_dir):
+    """
+    Remove slice files 1kb or less.
+
+    Args:
+        output_dir (pathlib.Path): The path to the output directory.
+
+    Returns:
+        None
+    """
+    for root, dirs, files in os.walk(output_dir):
+        for file in files:
+            if file.endswith('.json'):
+                path = os.path.join(root, file)
+                if os.path.getsize(path) <= 1024:
+                    os.remove(path)
+
+
 def main():
     """
-    Executes the main logic of the program.
-
-    This function performs the following steps:
-    1. Builds the command line arguments.
-    2. Sets up the set of programming languages.
-    3. Sets the output directory if it is the current directory.
-    4. Filters out excluded programming languages.
-    5. Checks and creates the necessary directories.
-    6. Reads the repository data from a CSV file.
-    7. Generates atom slices based on the repository data.
-
+    Runs the main function of the program.
     """
     args = build_args()
-    if args.output_dir == '.':
-        args.output_dir = Path.cwd()
-    langs = set(args.langs)
-    if args.elangs:
-        langs = langs - set(args.elangs)
-    if args.clone:
-        check_dirs(args.clone_dir, args.output_dir)
-    repo_data = read_csv(args.repo_csv, langs)
-    generate(
-        repo_data,
-        args.clone_dir,
-        args.output_dir,
-        args.slice_types,
-        args.clone,
-        args.debug_cmds,
-        args.skip_build
-    )
+    if args.cleanup:
+        cleanup(args.output_dir)
+    else:
+        generate(args)
 
 
 if __name__ == '__main__':
